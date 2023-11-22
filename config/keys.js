f7545f228d6e5dca6c82f76bc9eb704fb8916382
Delanyo Nutakor <nutakordelanyo2@gmail.com>
2023-11-21 21:50:16 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // MongoURI : 'mongodb+srv://delanyo_07:Neversaynever@cluster0.feik421.mongodb.net/?retryWrites=true&w=majority'
-    MongoURI : 'mongodb+srv://delanyo_07:Neversaynever@cluster0.feik421.mongodb.net/test?retryWrites=true&w=majority'
+    // MongoURI : 'mongodb+srv://delanyo_07:<password>@cluster0.feik421.mongodb.net/?retryWrites=true&w=majority'
+    MongoURI : 'mongodb+srv://delanyo_07:<password>@cluster0.feik421.mongodb.net/test?retryWrites=true&w=majority'
 } 
